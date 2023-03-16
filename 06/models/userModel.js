efbe43b07ccf76e494a976e11f3b883fd560016a
HakuMatsunoki <hakumatsunoki@gmail.com>
2023-03-16 23:33:23 +0200

    isModified hook fix

--- 06/models/userModel.js
+++ 06/models/userModel.js
@@ -33,6 +33,8 @@ const userSchema = new mongoose.Schema({
 
 // Pre save hook
 userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   // const passwordIsValid = await bcrypt.compare('Pass&2234', hashedPassword);
